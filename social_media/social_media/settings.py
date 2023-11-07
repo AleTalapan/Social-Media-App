25068675f3a2c73ac47eca4a0c69e35f9466808f
Alexandra <aletalapan@gmail.com>
2023-11-08 00:57:28 +0200

    logout+account settings

--- social_media/social_media/settings.py
+++ social_media/social_media/settings.py
@@ -129,3 +129,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+]
+
