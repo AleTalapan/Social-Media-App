52c9b0498df56a47aa12b62adb305b62e679f4d0
Alexandra <aletalapan@gmail.com>
2023-11-13 15:28:16 +0200

    feed+likes+profile

--- social_media/core/views.py
+++ social_media/core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.http import HttpResponse
-from .models import Profile, Post
+from .models import Profile, Post, LikePost
 from django.views.decorators.cache import never_cache
 
 
@@ -13,7 +13,9 @@ def index(request):
     if request.user.is_authenticated:
         user_object = User.objects.get(username=request.user.username)
         user_profile = Profile.objects.get(user=user_object)
-        return render(request, 'index.html', {'user_profile': user_profile})
+
+        posts = Post.objects.all()
+        return render(request, 'index.html', {'user_profile': user_profile, 'posts':posts})
     else:
         return redirect('signup')
     
@@ -32,6 +34,45 @@ def upload(request):
             return redirect('/')
     else:
         return redirect('signup')
+
+def like_post(request):
+    if request.user.is_authenticated:
+        username = request.user.username
+        post_id = request.GET.get('post_id')
+
+        post = Post.objects.get(id=post_id)
+
+        like_filter = LikePost.objects.filter(post_id=post_id, username=username).first()
+
+        if like_filter == None:
+            new_like = LikePost.objects.create(post_id=post_id, username=username)
+            new_like.save()
+            post.no_of_likes = post.no_of_likes+1
+            post.save()
+            return redirect('/')
+        else:
+            like_filter.delete()
+            post.no_of_likes = post.no_of_likes-1
+            post.save()
+            return redirect('/')
+    else:
+        return redirect('signup')
+    
+def profile(request, pk):
+    if request.user.is_authenticated:
+        user_object = User.objects.get(username=pk)
+        user_profile = Profile.objects.get(user=user_object)
+        user_posts = Post.objects.filter(user=pk)
+        user_post_length = len(user_posts)
+        context = {
+            'user_object': user_object,
+            'user_profile': user_profile,
+            'user_posts': user_posts,
+            'user_post_length': user_post_length,
+        }
+        return render(request, 'profile.html', context)
+    else:
+        return redirect('signup')   
 
 def settings(request):
     if request.user.is_authenticated:
